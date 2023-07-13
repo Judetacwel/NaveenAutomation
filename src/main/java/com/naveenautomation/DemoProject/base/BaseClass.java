c125dcc827cd9448e3f0a6016d9057a92985f20c
Jude <judetacwel007@gmail.com>
2023-07-13 17:54:24 -0400

    Updated codes

--- src/main/java/com/naveenautomation/DemoProject/base/BaseClass.java
+++ src/main/java/com/naveenautomation/DemoProject/base/BaseClass.java
@@ -59,7 +59,7 @@ public class BaseClass {
 	public void initialisation() {
 		
 		//String browserName = System.getProperty("browser")!=null?  System.getProperty("browser"): prop.getProperty("browser");
-		String browserName = System.getProperty("browser","chrome");
+		String browserName = System.getProperty("browser");
 		//String browserName = prop.getProperty("browser");
 		switch (browserName) {
 		case "Chrome":
